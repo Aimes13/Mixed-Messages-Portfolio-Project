3bccae110523589c2a94f20dbb81a77056f90769
Anne-Marie <108076177+Aimes13@users.noreply.github.com>
2022-08-29 00:36:42 +0800

    Add functionality to button

--- program.js
+++ program.js
@@ -2,12 +2,24 @@ let showMsg;
 
 const randomMsgDisplay = document.getElementById('genRandomMsg');
 const randomImgDisplay = document.getElementById('genRandomImg');
+const goodImgDisplay = document.getElementById('goodImg');
+const badImgDisplay = document.getElementById('badImg');
 const pressButton = document.getElementById('button');
 
 pressButton.addEventListener('click', () => {
     //Display the random message
-    randomMsgDisplay.innerText = msgGenerator.randomMsg();
+    //randomMsgDisplay.innerText = msgGenerator.randomMsg();
 
-    //Set the correct disabled state for the buttons
+    //Display the random image
+    if((msgGenerator.randomMsg()).includes('amazing') || (msgGenerator.randomMsg()).includes('miraculous') || (msgGenerator.randomMsg()).includes('charming') || (msgGenerator.randomMsg()).includes('breath-taking')) {
+        randomMsgDisplay.innerText = msgGenerator.randomMsg();
+        randomImgDisplay.innerText = goodImgDisplay;
+    }
+    else {
+        randomMsgDisplay.innerText = msgGenerator.randomMsg();
+        randomImgDisplay.innerText = badImgDisplay;
+    }; 
+
+    //Set the correct disabled state for the button
     pressButton.removeAttribute('disabled');
 });
